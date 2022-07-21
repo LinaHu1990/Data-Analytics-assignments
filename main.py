f10e2b0b11ef16b4ddc64b4bd5bb80c9463f6d51
LinaHu1990 <103107224+LinaHu1990@users.noreply.github.com>
2022-07-21 16:20:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,67 @@
-# Do not modify these lines
-__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
-__human_name__ = 'strings'
+__winc_id__ = "ae539110d03e49ea8738fd413ac44ba8"
+__human_name__ = "files"
 
-# Add your code after this line
-from ast import Str
-from asyncio import BaseTransport
-from email.utils import localtime
+from functools import cache
+import os
+import zipfile
+from pathlib import Path
 
-player0="Ruud Gullit"
-player1="Marco van Basten"
-goal_0=32
-goal_1=54
-scorers = player0 +" "+str(goal_0)+", "+player1+" "+ str(goal_1)
-report=f'{player0} scored in the {goal_0}nd minute\n{player1} scored in the {goal_1}th minute'
 
-#part2
-player="Ruud Gullit" 
-first_name=player[:player.find(" ")]
-last_name_len=len(player[player.find(" ")+1:])
-name_short=(player[0])+". "+(player[player.find(" ")+1:])
-chant=(first_name+"! ")*(len(first_name)-1) +first_name+"!"
-good_chant=(chant[-1] != " ")
+
+#part1 
+def clean_cache(): 
+    path = '/Users/linah/Winc/files/cache'
+    dir=os.path.exists(path)
+    if not dir: #if path doesn't exist create folder cache
+        os.mkdir(path)
+    else: #if path does exist remove files in folder cache
+        for files in os.listdir(path):
+            new_path = path+"/"+files
+            if os.path.isfile(new_path):
+                print("Deleting file:", new_path)
+                os.remove(new_path)
+    return 'cache folder is created and empty'
+            
+#print(clean_cache())
+
+#part2 #import zipfile module
+def cache_zip(zip_file_path,dir_path):
+    file = '/Users/linah/Winc/files/data.zip'
+    zip_obj=zipfile.ZipFile(file,"r")#question: why first create zip_object if it is already a zip object?
+    zip_obj.extractall('/Users/linah/Winc/files/cache') 
+    return 'zip file is unpacked'
+
+zip = '/Users/linah/Winc/files/data.zip'
+dir = '/Users/linah/Winc/files/cache'
+#print(cache_zip(zip,dir))
+
+#part3 #obtain absolute path cache folder, irrespective of os 
+#return list of all files in cache 
+def cached_files():
+    abs_path = os.path.abspath('/Users/linah/Winc/files/cache')
+    list_cached_files = []
+    for path in os.listdir(abs_path):
+        add_path =os.path.join(abs_path,path)
+        list_cached_files.append(add_path)
+    return list_cached_files
+
+#print(cached_files())
+#question: outcome should be list of all file paths? instead of list of all file names? 
+
+#part4 
+def find_password(list):
+    for textfile in list:
+        file = open(textfile,'r')
+        lines=file.readlines()
+        for item in lines:
+            item_into_list = item.split("\n")
+            for detail in item_into_list: 
+                if "password" in detail: 
+                    return detail[detail.find(" ")+1:]
+                    #alternative return detail[10:40]#question how to best only return password itself?                     
+
+print(find_password(cached_files()))
+
+
+     
+
