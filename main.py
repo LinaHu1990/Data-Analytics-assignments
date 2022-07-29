80886f1498bd6968492e74d789efa66edb5012ad
LinaHu1990 <103107224+LinaHu1990@users.noreply.github.com>
2022-07-29 20:39:30 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ __human_name__ = 'arguments'
 #part1 
 #define function greet w. arg name, greeting template
 
-def greet(name,greeting_template="Hello"):
-    greet2=print(f"{greeting_template}, {name}!")
-    return greet2
+def greet(name, greeting_template="Hello, <name>!"):
+    return greeting_template.replace("<name>",name)
 
-greet("Bob")
+print(greet("Bob"))
 
 #part2 
-def force(mass,body="earth"):       
+def force(mass, body = "earth"):       
     body_dictionary = {
         "sun":274.0,
         "jupiter":24.9,
@@ -25,16 +24,14 @@ def force(mass,body="earth"):
         "mars":3.7,
         "mercury":3.7,
         "moon":1.6,
-        "pluto":0.6,    }    
-    force_gravity = body_dictionary[body]*mass
-    return round(force_gravity,1)
-
-print(force(3,"sun"))
+        "pluto":0.6,}    
+    return round(body_dictionary[body] * mass, 1)
+    
+print(force(3, "sun"))
  
 #part3
 def pull(m1, m2, d):
-    G = 6.674*10**-11
-    calculation = (m1*m2/(d**2))*G
-    return calculation
-
-print(pull(0.1,1500,3))
+    G = 6.674 * 10**-11
+    return (m1 * m2 / (d**2)) * G
+   
+print(pull(0.1, 1500, 3))
